af90d3d64e1cfedd217ae1ebd5dc0bfc7300d472
baxacsharp <78537820+baxacsharp@users.noreply.github.com>
2021-04-02 22:19:51 +0200

    Api Added

--- build week/script.js
+++ build week/script.js
@@ -1,11 +1,59 @@
+const history = localStorage.getItem("query");
+let query = history ? history : "daft punk";
 
- 
-const searchbtn=()=>{
-  const searched = document.getElementById("searched")
-  const ex= document.getElementById("ex")
-  ex.style.display = "block"
-  searched.addEventListener("click", searchbtn)
-}
+const handleSearchQuery = (e) => {
+  // console.log(e.target.value);
+  query = e.target.value;
+  localStorage.setItem("query", e.target.value);
+  // console.log(query);
+};
+
+const search = async (q) => {
+  // console.log(q);
+  // console.log(
+  //   "https://striveschool-api.herokuapp.com/api/deezer/search?q=" + q
+  // );
+  let response = await fetch(
+    "https://striveschool-api.herokuapp.com/api/deezer/search?q=" + q
+  );
+  let body = await response.json();
+  let data = body.data;
+  console.log(data);
+  getDatas(data);
+};
+
+const searchBtn = document.getElementById("searchBtn");
+searchBtn.addEventListener("click", () => search(query));
+
+const getDatas = (musics) => {
+  console.log(musics);
+  const row = document.getElementById("card");
+  row.innerHTML = "";
+  musics.forEach((music) => {
+    row.innerHTML += getCardInfo(music);
+  });
+};
+const getCardInfo = (music) => {
+  return `
+  <div class="box d-flex flex-column col-sm-12 col-md-4 col-lg-2">
+    <span style="--i:1;"><a href="albumpage.html?albumId=${music.album.id}"><img class="img-fluid mx-auto mx-md-0" src=${music.album.cover_medium}>
+    </span>
+    <h6>${music.title_short}</h6>
+  </div></a>
+  `;
+};
+
+window.onload = () => {
+  search(query);
+  const handleInput = document.getElementById("SearchField");
+  handleInput.addEventListener("keyup", handleSearchQuery);
+};
+const searchbtn = () => {
+  const searched = document.getElementById("searched");
+  const ex = document.getElementById("ex");
+  ex.style.display = "block";
+  searched.addEventListener("click", searchbtn);
+};
 let previous = document.querySelector("#prev");
 let play = document.querySelector("#playBtn");
 let pause = document.querySelector("#pauseBtn");
